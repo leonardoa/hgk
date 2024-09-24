8f977362c5b2e3ef1ad429292fba0c6b1e311521
Leonardo Angelucci <mail@leonardo-angelucci.ch>
2024-09-24 22:37:23 +0200

    added js

--- sources/javascript/1/script.js
+++ sources/javascript/1/script.js
@@ -1,42 +1,52 @@
-var swiper = new Swiper(".mySwiper1", {
-  slidesPerView: "auto",
-  spaceBetween: 10,
-  centeredSlides: false,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  loop: false,
-  pagination: {
-    el: ".swiper-pagination1",
-    clickable: true,
-  },
-});
+function draw(predictions) {
+  let container = document.getElementById("container");
+  let rect = document.getElementById("rect");
+  let widthCanvas = document.getElementById("canvas").width;
+  let heightCanvas = document.getElementById("canvas").height;
+  let xFace, yFace, widthFace, heightFace;
+  let widthWindow = window.innerWidth;
+  let heightWindow = window.innerHeight;
+  let x,y;
 
-var swiper = new Swiper(".mySwiper2", {
-  slidesPerView: 2,
-  spaceBetween: 10,
-  centeredSlides: false,
-  autoplay: false,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination2",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+  if (predictions.length > 0) {
+    // control if we have at least an item with label face
+    let face = predictions.find((item) => item.label === "face");
 
-var swiper = new Swiper(".mySwiper3", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  centeredSlides: false,
-  autoplay: false,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination3",
-    clickable: true,
-  },
-});
+    // if we have a face
+    if (face) {
+
+      // get the coordinates of the face and size
+      xFace = face.bbox[0];
+      yFace = face.bbox[1];
+      widthFace = face.bbox[2];
+      heightFace = face.bbox[3];
+
+      // map the coordinates of the face from the canvas to the window
+      x = mapRange(xFace, 0, widthCanvas, 0, widthWindow);
+
+      //change the position of the rect and the colors
+      rect.style.opacity = `1`;
+      rect.style.left = `${x}px `;
+      rect.style.backgroundColor = "black";
+      rect.style.borderRadius = "0%";
+      rect.style.width = `${mapRange(widthFace, 0, widthCanvas, 0, widthWindow)}px`;
+      rect.style.height = `${mapRange(heightFace, 0, heightCanvas, 0, heightWindow)}px`;
+      rect.innerHTML = `x: ${xFace} <br> y: ${yFace} <br> width: ${widthFace} <br> height: ${heightFace}`;
+      
+      //change the color of the container if the face is on the right or on the left
+      if((xFace + (widthFace/2)) > widthCanvas/2){
+        container.style.backgroundColor = "rgb(0, 255, 0)";
+      }
+      else {
+        container.style.backgroundColor = "white";
+      }
+        
+    } 
+  }
+}
+
+// map function
+function mapRange(value, a, b, c, d) {
+  value = (value - a) / (b - a);
+  return c + value * (d - c);
+}
--- sources/javascript/2/script.js
+++ sources/javascript/2/script.js
@@ -1,47 +1,49 @@
-let row = document.querySelectorAll(".flex-container");
-//if mouse hover on the row show the image inside
-row.forEach((element) => {
-  //mouse hover on the row show the image inside
-  element.addEventListener("mouseover", (e) => {
-    if (!e.currentTarget.querySelector("img")) return;
-    e.currentTarget.querySelector("img").style.opacity = 1;
-  });
-  //mouse leave hide the image
-  element.addEventListener("mouseleave", (e) => {
-    if (!e.currentTarget.querySelector("img")) return;
-    e.currentTarget.querySelector("img").style.opacity = 0;
-  });
-});
+//volume and play/pause
+let video = document.getElementById("movie");
+let value = document.getElementById("value");
+let numOfImages = 7;
 
+function draw(predictions) {
+  let heightCanvas = document.getElementById("canvas").height;
+  let heightWindow = window.innerHeight;
 
+  //divide the screen
+  let screenSection = heightWindow / numOfImages;
+  if (predictions.length > 0) {
+    let close = predictions.filter((item) => item.label === "closed");
+    if (close.length >= 1) {
+      let xClose = close[0].bbox[0];
+      let yClose = close[0].bbox[1];
 
+      let yCloseMap = mapRange(yClose, 0, heightCanvas, 0, heightWindow);
+      const sectionNumber = findImageSection(yCloseMap, screenSection);
+      value.innerHTML = sectionNumber;
+      value.style.fontSize = "50" * sectionNumber+ "px";
+    }
+  }
+}
 
+// map function
+function mapRange(value, a, b, c, d) {
+  value = (value - a) / (b - a);
+  return c + value * (d - c);
+}
 
-//images that follow the mouse
-let row2 = document.querySelectorAll(".flex-mouse-follow");
-//if mouse hover on the row show the image inside
-row2.forEach((element) => {
-  //mouse hover on the row show the image inside
-  element.addEventListener("mouseover", (e) => {
-    if (!e.currentTarget.querySelector("img")) return;
-    e.currentTarget.querySelector("img").style.opacity = 1;
-  });
-
-  //mouse leave hide the image
-  element.addEventListener("mouseleave", (e) => {
-    if (!e.currentTarget.querySelector("img")) return;
-    e.currentTarget.querySelector("img").style.opacity = 0;
-  });
-
-  //follow the mouse
-  element.addEventListener("mousemove", (e) => {
-    if (!e.currentTarget.querySelector("img")) return;
-    let x = e.clientX;
-    let y = e.clientY;
-
-    e.currentTarget.querySelector("img").style.opacity = 1;
-    //do it with top and left
-    e.currentTarget.querySelector("img").style.top = `${y}px`;
-    e.currentTarget.querySelector("img").style.left = `${x}px`;
-  });
-});
+//this function will find the section of the image
+function findImageSection(mouseY, screenSection, section = 1) {
+  // Check if we are in the first section
+  if (section === 1 && mouseY < screenSection) {
+    return section;
+  }
+  // Check if we are in the current section
+  else if (mouseY < screenSection * section) {
+    return section;
+  }
+  // Otherwise, move to the next section
+  else {
+    if (section === numOfImages) {
+      return section;
+    }
+    return findImageSection(mouseY, screenSection, section + 1);
+  }
+}
