7e1953a38e3c744f57921c5405654b8b9172eabc
Leonardo Angelucci <mail@leonardo-angelucci.ch>
2024-10-07 16:11:12 +0200

    update

--- sources/javascript/10/script.js
+++ sources/javascript/10/script.js
@@ -1,18 +1,25 @@
 let i = 0; // Index for characters in a string
 let arrIndex = 0; // Index for strings in the array
-let txtArray = ['Unica77', 'Brown', 'Ruder', 'Geigy Duplex', 'Riforma Mono']; // Array of strings
+let txtArray = ["Unica77", "Brown", "Ruder", "Geigy Duplex", "Riforma Mono"]; // Array of strings
 let speed = 120; // Typing speed
 
 function typeWriter() {
+  //if the index is less than the length of the string
   if (arrIndex < txtArray.length) {
+    //if the index is less than the length of the string
     if (i < txtArray[arrIndex].length) {
-      document.getElementById("textTypeWriting").innerHTML += txtArray[arrIndex].charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    } else {
+      document.getElementById("textTypeWriting").innerHTML +=
+        txtArray[arrIndex].charAt(i); //add the character to the string
+      i++; //increment the index by 1
+      setTimeout(typeWriter, speed); // call the function again after the speed
+    }
+    //otherwise, start deleting the text
+    else {
       setTimeout(typeDelete, 1000); // Start deleting after the text is typed
     }
-  } else {
+  }
+  //otherwise, reset the index and start typing again
+  else {
     arrIndex = 0; // Reset to first string in array after last one is deleted
     setTimeout(typeWriter, 1000); // Loop back to start typing again
   }
@@ -20,7 +27,10 @@ function typeWriter() {
 
 function typeDelete() {
   if (i > 0) {
-    document.getElementById("textTypeWriting").innerHTML = txtArray[arrIndex].substring(0, i - 1);
+    // Delete the character at the index i
+    document.getElementById("textTypeWriting").innerHTML = txtArray[
+      arrIndex
+    ].substring(0, i - 1);
     i--;
     setTimeout(typeDelete, speed);
   } else {
